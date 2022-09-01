b6bc24ab6f9acb3555e00c579cfd5e12146d50d6
samuelmayna <samuelmayna@gmail.com>
2022-09-01 11:34:29 +0300

    Added handler and the 404 response handler

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,33 @@ app.use(cors());
 app.use(express.json());
 app.use("api", routes);
 
+app.use(nonExistantApiEndpoint);
+
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log("Server listening of port", PORT);
 });
+
+function errorHandler(
+  error: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.log(error);
+  res.json(500).json({
+    message:
+      "Some internal server error occured.Sorry for any inconvinience cause. Please try again later.",
+  });
+}
+
+function nonExistantApiEndpoint(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  res.json(404).json({
+    message: "Page not found.Check for typos in the url",
+  });
+}
