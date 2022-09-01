95f0b611a9596f6471fa087d000bbeedbc38510d
samuelmayna <samuelmayna@gmail.com>
2022-09-01 11:15:10 +0300

    Added the first controller of getting all the products from the database

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,5 +1,13 @@
 import { NextFunction, Request, Response } from "express";
+import product from "../db/product";
 
 export default {
-  getProducts: (req: Request, res: Response, next: NextFunction) => {},
+  getProducts: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const products = await product.getAllProduct();
+      res.status(201).json(products);
+    } catch (error) {
+      next(error);
+    }
+  },
 };
